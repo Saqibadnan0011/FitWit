853127ee2a15754b128c008f241306fbd4b00c3f
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-03 22:32:32 +0500

    Updated

---
+++ FitWit/FitWit/FitWitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitWitApp.swift
+//  FitWit
+//
+//  Created by apple on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitWitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
