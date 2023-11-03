9b643a1f42bbc6f190fb22f334a51e45ae4fca71
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-04 03:59:14 +0500

    Completed FlashScreen design

--- FitWit/FitWit/Views/ContentView.swift
+++ FitWit/FitWit/Views/ContentView.swift
@@ -15,10 +15,14 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 BackgroundView()
+                Circle()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(.blue).blur(radius: 14).opacity(0.2)
+                    .padding(.bottom, 350)
                 Image("first")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 350, height: 350)
+                    .frame(width: 300, height: 300)
                     .padding(.bottom, 350)
                 VStack(spacing: 20) {
                     Text("Make Yourself Stronger then Your Excuses!")
@@ -28,7 +32,7 @@ struct ContentView: View {
                         .bold()
                         .padding()
                         .padding(.top, 340)
-                    Text("Get Started Your Body Maintainance through out all Watching Your Activity and Your Daily Progress")
+                    Text("Get Started Your Body Maintainance through out all Watching Your Activity and your Daily Progress")
                         .fixedSize(horizontal: false, vertical: true)
                         .foregroundColor(.white).opacity(0.6)
                         .font(.callout)
