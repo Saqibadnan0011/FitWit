750bf2749d13ecf7be6ada9159e6c88b6542d725
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-11-14 23:50:02 +0500

    Replace some features

--- FitWit/FitWit/Views/MatchView.swift
+++ FitWit/FitWit/Views/MatchView.swift
@@ -13,13 +13,16 @@ struct MatchView: View {
     
     var body: some View {
         ZStack {
+            Color.black
+                .ignoresSafeArea(.all)
             if !show {
+                Spacer()
                 VStack (alignment: .leading, spacing: 12) {
                     Text("Daily Progress")
                         .font(.title.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Steps and More".uppercased())
+                    Text("Steps and more".uppercased())
                         .font(.footnote.bold())
                         .matchedGeometryEffect(id: "subtitle", in: namespace)
                     Text("See your daily progress and make more your body fit and healthy")
@@ -29,32 +32,49 @@ struct MatchView: View {
                 .padding(20)
                 .foregroundStyle(.white)
                 .background(
-                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                    Image("Card1.0")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace)
                 )
+                .mask(
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                )
+                .frame(height: 300)
                 .padding(20)
             } else {
-                VStack (alignment: .leading, spacing: 12) {
-                    Spacer()
-                    Text("Daily Progress")
-                        .font(.title.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("See your daily progress and make more your body fit and healthy".uppercased())
-                        .font(.footnote.bold())
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    Text("See your daily progress and make more your body fit and healthy")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                }
-                .padding(20)
-                .foregroundStyle(.white)
-                .background(
-                    Color.gray.matchedGeometryEffect(id: "background", in: namespace)
+                ScrollView {
+                    VStack (alignment: .leading, spacing: 12) {
+                        Spacer()
+                        Text("Daily Progress")
+                            .font(.title.weight(.bold))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("Steps and more".uppercased())
+                            .font(.footnote.bold())
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        Text("See your daily progress and make more your body fit and healthy")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "text", in: namespace)
+                    }
+                    .padding(20)
+                    .foregroundStyle(.white)
+                    .background(
+                        Image("Card1.0")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask(
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
                 )
+                }
             }//else
         }
         .onTapGesture {
-            withAnimation {
+            withAnimation (.spring(response: 0.6, dampingFraction: 0.8)) {
                 show.toggle()
             }
         }//tap
